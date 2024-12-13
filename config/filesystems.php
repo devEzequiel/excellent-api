64c9f43c248714da8ad67d69eca61b80d3725e6c
Ezequiel Oliveira <esofinney@gmail.com>
2024-12-13 02:34:54 -0300

    feat/products - Created controller to upload image

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,10 +39,9 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => storage_path('app/public'), // Diretório onde as imagens serão salvas
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
-            'throw' => false,
         ],
 
         's3' => [
